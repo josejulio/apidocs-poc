f8ea177ff4cbe65a76cec8a87f24bdf433507865
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-01-24 12:55:19 -0700

    Adding patternfly  - also adds the response examples

--- src/components/APIDoc/ApiDoc.tsx
+++ src/components/APIDoc/ApiDoc.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {OpenAPIV3} from "openapi-types";
-import {SecurityScheme} from "./SecuritySchemes";
 import {deRef} from "../../utils/Openapi";
 import {Operation} from "./Operation";
+import {TextContent, Text, TextVariants, Stack, StackItem} from "@patternfly/react-core";
+import {ServerList} from "./ServerList";
+import {SecuritySchemeList} from "./SecuritySchemeList";
 
 interface ApiDocProps {
     openapi: OpenAPIV3.Document;
@@ -11,34 +13,36 @@ interface ApiDocProps {
 export const ApiDoc: React.FunctionComponent<ApiDocProps> = props => {
     const { openapi } = props;
 
-    return <>
-        <h1>{ openapi.info.title } v{ openapi.info.version}</h1>
-        <div>{ openapi.info.description }</div>
+    return <Stack hasGutter>
+        <StackItem>
+            <TextContent>
+                <Text component={TextVariants.h1}>
+                    { openapi.info.title } v{ openapi.info.version}
+                </Text>
+                <Text component={TextVariants.p}>
+                    { openapi.info.description }
+                </Text>
+            </TextContent>
+        </StackItem>
         { openapi.servers && (
-            <div>
-                <span>Base URLs:</span>
-                <ul>
-                    { openapi.servers.map((s, index) => <React.Fragment key={index}>{s.url}</React.Fragment>)}
-                </ul>
-            </div>
+            <StackItem>
+                <ServerList servers={openapi.servers}/>
+            </StackItem>
         )}
         { openapi.components?.securitySchemes && (
-            <div>
-                <h1>Authentication</h1>
-                <ul>
-                    {Object.values(openapi.components.securitySchemes).map((scheme, index) => (
-                        <SecurityScheme key={index} securityScheme={deRef(scheme, openapi)}/>
-                    ))}
-                </ul>
-            </div>
+            <StackItem>
+                <SecuritySchemeList schemes={Object.values(openapi.components.securitySchemes).map(s => deRef(s, openapi))} />
+            </StackItem>
         )}
         { Object.entries(openapi.paths).map(([path, pathObject]) => {
             return Object.entries(
                 // Looks like openapi v3.1 supports components here as well
                 pathObject as Record<OpenAPIV3.HttpMethods, OpenAPIV3.OperationObject>
             ).map(([verb, operation]) =>
-                <Operation key={`${verb} ${path}`} verb={ verb } path={ path } operation={ operation } document={ openapi }/>
+                <StackItem>
+                    <Operation key={`${verb} ${path}`} verb={ verb } path={ path } operation={ operation } document={ openapi }/>
+                </StackItem>
             );
         })}
-    </>;
+    </Stack>;
 }
--- src/components/APIDoc/Operation.tsx
+++ src/components/APIDoc/Operation.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {OpenAPIV3} from "openapi-types";
-import {deRef} from "../../utils/Openapi";
+import {buildExample, deRef} from "../../utils/Openapi";
+import {Stack, StackItem, Text, TextContent, TextVariants} from "@patternfly/react-core";
+import {TableComposable, Tbody, Td, Thead, Tr} from "@patternfly/react-table";
+import {ExampleResponse} from "./ExampleResponse";
 
 export interface OperationProps {
     verb: string;
@@ -11,59 +14,76 @@ export interface OperationProps {
 export const Operation: React.FunctionComponent<OperationProps> = ({verb, path, operation, document}) => {
     const parameters = (operation.parameters || []).map(p => deRef(p, document));
     const responseMap = Object.entries(operation.responses);
-    return <>
-        <h1>{ operation.summary }</h1>
-        <div>{verb} {path}</div>
-        <div>{ operation.description }</div>
-        { parameters.length > 0 && <>
-            <h2>Parameters</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Name</td>
-                        <td>In</td>
-                        <td>Type</td>
-                        <td>Required</td>
-                        <td>Description</td>
-                    </tr>
-                </thead>
-                <tbody>
-                { parameters.map(p => (
-                    <tr>
-                        <td>{p.name}</td>
-                        <td>{p.in}</td>
-                        <td>{getType(p.schema, document)}</td>
-                        <td>{p.required ? 'Yes' : 'No'}</td>
-                        <td>{p.description}</td>
-                    </tr>
-                )) }
-                </tbody>
-            </table>
-        </>}
-        { responseMap.length > 0 && <>
-            <h2>Responses</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Status</td>
-                        <td>Description</td>
-                        <td>Schema</td>
-                    </tr>
-                </thead>
-                <tbody>
+    const responseExample = React.useMemo(() => {
+        const example = buildExample(operation.responses, document);
+        if (example) {
+            return JSON.stringify(example, undefined, 2);
+        }
+
+        return undefined;
+    }, [ operation.responses, document]);
+
+    return <Stack hasGutter>
+        <StackItem>
+            <TextContent>
+                <Text component={TextVariants.h2}>{ operation.summary }</Text>
+                <Text component={TextVariants.p}>{verb.toUpperCase()} {path}</Text>
+                <Text component={TextVariants.p}>{operation.description}</Text>
+            </TextContent>
+        </StackItem>
+        { parameters.length > 0 && <StackItem>
+            <TextContent>
+                <Text component={TextVariants.h3}>Parameters</Text>
+            </TextContent>
+            <TableComposable>
+                <Thead>
+                    <Tr>
+                        <Td>Name</Td>
+                        <Td>In</Td>
+                        <Td>Type</Td>
+                        <Td>Required</Td>
+                        <Td>Description</Td>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {parameters.map(((p, index) => (
+                        <Tr key={index}>
+                            <Td>{p.name}</Td>
+                            <Td>{p.in}</Td>
+                            <Td>{getType(p.schema, document)}</Td>
+                            <Td>{p.required ? 'Yes' : 'No'}</Td>
+                            <Td>{p.description}</Td>
+                        </Tr>
+                    )))}
+                </Tbody>
+            </TableComposable>
+        </StackItem> }
+        { responseMap.length > 0 && <StackItem>
+            <TextContent>
+                <Text component={TextVariants.h3}>Responses</Text>
+            </TextContent>
+            <TableComposable>
+                <Thead>
+                    <Tr>
+                        <Td>Status</Td>
+                        <Td>Description</Td>
+                        <Td>Schema</Td>
+                    </Tr>
+                </Thead>
+                <Tbody>
                     {responseMap.map(([code, response]) => {
                         const dResponse = deRef(response, document);
-                        return <tr>
-                            <td>{code}</td>
-                            <td>{dResponse.description}</td>
-                            <td>{getResponseSchema(dResponse, document)}</td>
-                        </tr>;
+                        return <Tr key={code}>
+                            <Td>{code}</Td>
+                            <Td>{dResponse.description}</Td>
+                            <Td>{getResponseSchema(dResponse, document)}</Td>
+                        </Tr>;
                     })}
-                </tbody>
-            </table>
-        </>}
-
-    </>;
+                </Tbody>
+            </TableComposable>
+            {responseExample && <ExampleResponse response={responseExample} />}
+        </StackItem> }
+    </Stack>;
 }
 
 const getResponseSchema = (response: OpenAPIV3.ResponseObject, document: OpenAPIV3.Document) => {
